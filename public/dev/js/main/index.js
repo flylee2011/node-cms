da3cc42ea82fafa771aba150517b2a67c6dada8e
liyifei <liyifei@le.com>
2017-05-31 18:23:25 +0800

    update

--- public/dev/js/main/index.js
+++ public/dev/js/main/index.js
@@ -1,3 +1,9 @@
+/**
+ * @fileoverview js入口
+ * @author liyifei<yifei@zoocer.com>
+ * @date 2017/05
+ */
+// 热替换
 if (module.hot) {
     module.hot.accept();
 }
@@ -6,4 +12,4 @@ require('../../css/index.css');
 require('vue');
 require('../mods/root.js');
 
-console.log('main/index.js, test 3');
+console.log('main/index.js, test 1');
